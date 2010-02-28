3632e127c69dfe2b43aee26d52d8adbb62f2ea24
Alexandre Santos <alexandre@diariodecodigos.info>
2010-02-28 20:04:57 -0300

    upando...

---
+++ divisibilidade.h
@@ -0,0 +1,30 @@
+#include <string.h>
+
+/**
+  * Verifica se o número passado como argumetno é divisível por 2, 
+  * se ele é um número par ele é divisível por 2, para verificar se o número é par
+  * é necessário dividí-lo por 2, se o resto for 0, o número é par e divisível por 2
+  * @return bool
+  */
+div2(int num){
+         if(num % 2 == 0){
+                return 1;
+         }else{
+               return 0;
+         }         
+}
+         
+/**
+  * Verifica se o número é divisível por 3
+  * neste caso é somado os valores absolutos do número passado como argumento
+  * e se essa soma tiver um número divisível por 3, ele é divisível por 3
+  * Exemplo: 243 -> 2 + 4 + 3 = 9 
+  * 9 é divisível por 9? Sim...
+  * @return bool
+  */
+div3(char num[]){
+        char number[6];
+        int nuns; 
+        
+        return strlen(num);
+}
---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "divisibilidade.h"
+
+int main(int argc, char *argv[])
+{
+  int num;
+  int result;
+  char str[];
+
+  // pega número
+  printf("Digite um numero: ");
+  scanf("%d", &num);
+  
+  result = div2(num);
+  
+  if(result != 0){
+            printf("your number is divisible by 2, it should even.");
+  }else{
+        printf("your number is not divisible by 2, it should odd.");
+  }
+  
+  printf("\n\n ----------------------------------------------- \n\n");
+  
+  printf("Digte uma string: ");
+  //scanf("%s", &str);
+  gets(str);
+  printf("\n the length your string is: %d", div3(str)); 
+
+  printf("\n\n");
+  system("PAUSE");
+  return 0;
+}
